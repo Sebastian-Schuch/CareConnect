0c574070dd8e9484d7671e18ff02833db812b795
Ryan Foster <e12222173@student.tuwien.ac.at>
2024-05-06 11:45:32 +0200

    added @Secured annotation to patient create endpoint

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/PatientEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/PatientEndpoint.java
@@ -3,6 +3,7 @@ package at.ac.tuwien.sepr.groupphase.backend.endpoint;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.PatientCreateDto;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.PatientDto;
 import at.ac.tuwien.sepr.groupphase.backend.service.PatientService;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class PatientEndpoint {
         this.patientService = patientService;
     }
 
+    @Secured("ROLE_USER")
     @PostMapping
     public PatientDto create(@RequestBody PatientCreateDto toCreate) {
         return this.patientService.createPatient(toCreate);
