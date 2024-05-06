d7fe3f727999a79d270163e33195393937b7c0e2
p12034156 <e12034156@student.tuwien.ac.at>
2024-05-06 14:32:26 +0200

    added basic sql database setup

--- backend/src/main/resources/sql/createSchema.sql
+++ backend/src/main/resources/sql/createSchema.sql
@@ -1,11 +1,124 @@
+-- section users
+-- todo: redname table
+CREATE TABLE IF NOT EXISTS credentials
+(
+    uid BIGINT AUTO_INCREMENT PRIMARY KEY,
+    password VARCHAR(255),
+    isActive Boolean,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    username VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS doctor
+(
+    uid BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_ref BIGINT,
+    FOREIGN KEY (uid) REFERENCES user(uid)
+);
+
+CREATE TABLE IF NOT EXISTS admin
+(
+    uid BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_ref BIGINT,
+    FOREIGN KEY (uid) REFERENCES user(uid)
+);
+
+CREATE TABLE IF NOT EXISTS secretary
+(
+    uid BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_ref BIGINT,
+    FOREIGN KEY (uid) REFERENCES user(uid)
+);
+
 CREATE TABLE IF NOT EXISTS patient
 (
     uid BIGINT AUTO_INCREMENT PRIMARY KEY,
     svnr INT,
-    name VARCHAR(255),
-    firstname VARCHAR(255),
-    username VARCHAR(255),
     password VARCHAR(255),
     email VARCHAR(255),
     active BOOLEAN
-)
+);
+
+-- section static data
+CREATE TABLE IF NOT EXISTS allergies
+(
+    uid BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+
+CREATE TABLE IF NOT EXISTS medicine
+(
+    uid BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS ambulance
+(
+    uid BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    capacity BIGINT
+);
+
+-- table for each day
+CREATE TABLE IF NOT EXISTS opening_hours
+(
+    ambulance_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    day VARCHAR(10),
+    opening_time TIME,
+    closing_time TIME,
+
+    FOREIGN KEY (ambulance_id) REFERENCES ambulance(uid)
+);
+
+CREATE TABLE IF NOT EXISTS station
+(
+    uid BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    capacity BIGINT
+);
+
+CREATE TABLE IF NOT EXISTS residence
+(
+    uid BIGINT AUTO_INCREMENT PRIMARY KEY,
+    patient_uid BIGINT,
+    startDate DATETIME,
+    endDate DATETIME,
+
+    FOREIGN KEY(patient_uid) REFERENCES Patient(uid)
+);
+
+-- section dynamic data
+CREATE TABLE IF NOT EXISTS appointment
+(
+    uid BIGINT AUTO_INCREMENT PRIMARY KEY,
+    startDate DATETIME,
+    endDate DATETIME,
+    notes VARCHAR(2048), -- TODO: discuss max note length
+    ambulance_id BIGINT,
+
+    FOREIGN KEY (ambulance_id) REFERENCES ambulance(uid)
+);
+
+CREATE TABLE IF NOT EXISTS treatment
+(
+    uid BIGINT AUTO_INCREMENT PRIMARY KEY,
+    startDate DATETIME,
+    endDate DATETIME,
+    doctor BIGINT,
+    notes VARCHAR(2048),
+
+    FOREIGN KEY (doctor) REFERENCES doctor(uid)
+);
+
+CREATE TABLE IF NOT EXISTS messages
+(
+    sender BIGINT,
+    treatment BIGINT,
+    message VARCHAR(2048),
+    date DATETIME,
+
+    FOREIGN KEY(treatment) REFERENCES Treatment(uid),
+    FOREIGN KEY(sender) REFERENCES user(uid)
+);
