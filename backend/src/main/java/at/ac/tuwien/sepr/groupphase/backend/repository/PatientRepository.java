9f9345bd83315eaa07db54515bd9d70416edd6fa
Ryan Foster <e12222173@student.tuwien.ac.at>
2024-05-07 20:09:53 +0200

    fix: code style

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/PatientRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/PatientRepository.java
@@ -3,7 +3,6 @@ package at.ac.tuwien.sepr.groupphase.backend.repository;
 import at.ac.tuwien.sepr.groupphase.backend.entity.Patient;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
