017f47f413bc15ccaa95d0831ddb3bc76b97c730
Noah Oguamalam <e12225111@student.tuwien.ac.at>
2024-05-07 18:15:03 +0200

    feat: switched validation removed validation from credential dto and added it to secretary dto

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/SecretaryDtoCreate.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/SecretaryDtoCreate.java
@@ -1,8 +1,28 @@
 package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
 public record SecretaryDtoCreate(
+    //TODO:
+    //other message for NotBlank?
+    @NotEmpty(message = "Email cannot be empty")
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Please provide a valid email address")
+    @Pattern(regexp = "[^@]+@[^@]+\\.[^@.]+", message = "Please provide a valid email address")
     String email,
+
+    @NotEmpty(message = "First name cannot be empty")
+    @NotBlank(message = "First name cannot be empty")
+    @Max(value = 255, message = "First name cannot be longer than 255 characters")
     String firstname,
+
+    @NotEmpty(message = "Last name cannot be empty")
+    @NotBlank(message = "Last name cannot be empty")
+    @Max(value = 255, message = "Last name cannot be longer than 255 characters")
     String lastname
 ) {
 }
