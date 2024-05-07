386f8837539a8fc52de87629813f91960db5f942
Noah Oguamalam <e12225111@student.tuwien.ac.at>
2024-05-07 18:08:09 +0200

    feat: slightly changed validation

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/CredentialCreateDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/CredentialCreateDto.java
@@ -3,24 +3,26 @@ package at.ac.tuwien.sepr.groupphase.backend.endpoint.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 
 public record CredentialCreateDto(
+    //TODO:
+    //other message for NotBlank?
 
+    @NotEmpty(message = "Email cannot be empty")
     @NotBlank(message = "Email cannot be empty")
-    @NotNull(message = "Email address is required")
     @Email(message = "Please provide a valid email address")
     @Pattern(regexp = "[^@]+@[^@]+\\.[^@.]+", message = "Please provide a valid email address")
     String email,
 
+    @NotEmpty(message = "First name cannot be empty")
     @NotBlank(message = "First name cannot be empty")
-    @NotNull(message = "First name is required")
     @Max(value = 255, message = "First name cannot be longer than 255 characters")
     String firstname,
 
+    @NotEmpty(message = "Last name cannot be empty")
     @NotBlank(message = "Last name cannot be empty")
-    @NotNull(message = "Last name is required")
     @Max(value = 255, message = "Last name cannot be longer than 255 characters")
     String lastname
 ) {
