69bf1f0c72891881490f823d31a2090801692fce
Noah Oguamalam <e12225111@student.tuwien.ac.at>
2024-05-08 12:52:11 +0200

    feat: fully implemented Secretary Service

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/SecretaryServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/SecretaryServiceImpl.java
@@ -1,15 +1,52 @@
 package at.ac.tuwien.sepr.groupphase.backend.service.impl;
 
-import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.SecretaryCreateDto;
-import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.SecretaryDetailDto;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.SecretaryDtoCreate;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.SecretaryDtoDetail;
+import at.ac.tuwien.sepr.groupphase.backend.endpoint.mapper.SecretaryMapper;
+import at.ac.tuwien.sepr.groupphase.backend.entity.Secretary;
+import at.ac.tuwien.sepr.groupphase.backend.repository.SecretaryRepository;
+import at.ac.tuwien.sepr.groupphase.backend.service.CredentialService;
 import at.ac.tuwien.sepr.groupphase.backend.service.SecretaryService;
+import at.ac.tuwien.sepr.groupphase.backend.type.Role;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.lang.invoke.MethodHandles;
 
 @Service
 public class SecretaryServiceImpl implements SecretaryService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private final CredentialService credentialService;
+    private final SecretaryRepository secretaryRepository;
+    private final SecretaryMapper secretaryMapper;
+
+    public SecretaryServiceImpl(CredentialService credentialService, SecretaryRepository secretaryRepository, SecretaryMapper secretaryMapper) {
+
+        this.credentialService = credentialService;
+        this.secretaryRepository = secretaryRepository;
+        this.secretaryMapper = secretaryMapper;
+    }
+
     @Override
-    public SecretaryDetailDto create(SecretaryCreateDto toCreate) {
-        return null;
+    public SecretaryDtoDetail create(SecretaryDtoCreate toCreate) {
+        LOG.trace("create{}", toCreate);
+        return insertSecretaryData(toCreate);
+    }
+
+    /**
+     * Write the secretary given into the repository.
+     *
+     * @param toCreate the secretary to write into the repository
+     * @return the secretary that has been written into the repository
+     */
+    public SecretaryDtoDetail insertSecretaryData(SecretaryDtoCreate toCreate) {
+        LOG.debug("insertSecretaryData{}", toCreate);
+        Secretary secretary = new Secretary();
+        secretary.setCredential(credentialService.createCredentialEntity(toCreate.toCredentialDtoCreate(), Role.SECRETARY));
+        return secretaryMapper.secretaryEntityToSecretaryDtoDetail(secretaryRepository.save(secretary));
     }
 }
 
