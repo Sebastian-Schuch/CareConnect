17208f8414ee92187473399594faaad4cfd22f5e
Noah Oguamalam <e12225111@student.tuwien.ac.at>
2024-05-09 19:11:14 +0200

    fix: used proper BASE_PATH and added new methods

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/SecretaryEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/SecretaryEndpoint.java
@@ -8,6 +8,8 @@ import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +17,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.invoke.MethodHandles;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = SecretaryEndpoint.BASE_PATH)
 public class SecretaryEndpoint {
-    static final String BASE_PATH = "/api/secretary";
+    static final String BASE_PATH = "/api/secretaries";
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final SecretaryService secretaryService;
 
@@ -33,14 +36,43 @@ public class SecretaryEndpoint {
      * @param toCreate the data for the secretary to create
      * @return the created secretary
      */
+    
+    //@Secured("ADMIN")
     @PermitAll
-    //@Secured("ROLE_ADMIN");
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public SecretaryDtoDetail create(@Valid @RequestBody SecretaryDtoCreate toCreate) {
         LOG.info("POST" + BASE_PATH);
         LOG.debug("Body of request:\n{}", toCreate);
         return this.secretaryService.create(toCreate);
+    }
+
+    /**
+     * The get endpoint for the secretary.
+     *
+     * @param id the id of secretary requested
+     * @return the secretary requested
+     */
+
+    @PermitAll
+    @GetMapping({"/{id}"})
+    //@Secured({"ADMIN", "PATIENT", "SECRETARY", "DOCTOR"})
+    public SecretaryDtoDetail getById(@PathVariable("id") long id) {
+        LOG.info("GET" + BASE_PATH + "/{}", id);
+        return secretaryService.getById(id);
+    }
+
+    /**
+     * Get all secretaries from the repository.
+     *
+     * @return a list of all secretaries
+     */
+    @PermitAll
+    //@Secured({"ADMIN", "PATIENT", "SECRETARY", "DOCTOR"})
+    @GetMapping
+    public List<SecretaryDtoDetail> getAll() {
+        LOG.info("GET " + BASE_PATH);
+        return this.secretaryService.getAllSecretaries();
     }
 
 
