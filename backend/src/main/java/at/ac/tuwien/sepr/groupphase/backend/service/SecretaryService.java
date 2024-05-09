ec800891e149da20d42ec1becc417a409cbe543b
Noah Oguamalam <e12225111@student.tuwien.ac.at>
2024-05-09 19:04:59 +0200

    feat: implemented new methods added getById, getAll

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/SecretaryService.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/SecretaryService.java
@@ -3,6 +3,8 @@ package at.ac.tuwien.sepr.groupphase.backend.service;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.SecretaryDtoCreate;
 import at.ac.tuwien.sepr.groupphase.backend.endpoint.dto.SecretaryDtoDetail;
 
+import java.util.List;
+
 public interface SecretaryService {
 
     /**
@@ -12,4 +14,20 @@ public interface SecretaryService {
      * @return the created secretary
      */
     SecretaryDtoDetail create(SecretaryDtoCreate toCreate);
+
+    /**
+     * Get the specified secretary.
+     *
+     * @param id the id of the secretary requested
+     * @return the secretary with the id given
+     */
+    SecretaryDtoDetail getById(Long id);
+
+    /**
+     * Get all patients from repository.
+     *
+     * @return the list of all patients
+     */
+    List<SecretaryDtoDetail> getAllSecretaries();
+
 }
