4546f07d4749b86f6ffe5ecad885fb04ea8f83b1
Noah Oguamalam <e12225111@student.tuwien.ac.at>
2024-05-09 19:14:07 +0200

    fix: adjusted Secretary Entity Id

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/Secretary.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/entity/Secretary.java
@@ -1,12 +1,14 @@
 package at.ac.tuwien.sepr.groupphase.backend.entity;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 
 import java.io.Serializable;
@@ -15,19 +17,21 @@ import java.io.Serializable;
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Secretary implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "secretary_id")
+    private Long secretaryId;
+
     @OneToOne(cascade = CascadeType.ALL)
-    @MapsId
     @JoinColumn(name = "id")
     private Credential credential;
-    private Long id;
+
 
     public void setId(Long id) {
-        this.id = id;
+        this.secretaryId = id;
     }
-
-    @Id
+    
     public Long getId() {
-        return id;
+        return secretaryId;
     }
 
     public Credential getCredential() {
