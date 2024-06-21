ff5c06372cda1b64421ad5cfec2bbb61e47e9061
Chris Anger <e12120520@student.tuwien.ac.at>
2024-06-21 12:31:32 +0000

    merge 'feat/26-edit-outpatient-departments' into 'master'

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/OutpatientDepartmentRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/OutpatientDepartmentRepository.java
@@ -1,6 +1,9 @@
 package at.ac.tuwien.sepr.groupphase.backend.repository;
 
 import at.ac.tuwien.sepr.groupphase.backend.entity.OutpatientDepartment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +11,15 @@ import java.util.List;
 
 @Repository
 public interface OutpatientDepartmentRepository extends JpaRepository<OutpatientDepartment, Long> {
+    /**
+     * Returns all outpatient departments of the given page from the db.
+     *
+     * @param spec     the specification to filter the outpatient departments
+     * @param pageable the page to get
+     * @return all outpatient departments of the page.
+     */
+    Page<OutpatientDepartment> findAll(Specification<OutpatientDepartment> spec, Pageable pageable);
+    
     List<OutpatientDepartment> findAllByActiveTrue();
 
     OutpatientDepartment findByIdAndActiveTrue(Long id);
