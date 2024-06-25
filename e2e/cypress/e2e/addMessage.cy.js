7249de4f247ef3f27f2b15f6c72c9c2e6c92d7ff
Sebastian Schuch <e12222156@student.tuwien.ac.at>
2024-06-25 12:18:51 +0000

    Add cypress end to end testing

--- e2e/cypress/e2e/addMessage.cy.js
+++
@@ -1,9 +0,0 @@
-context('add message', () => {
-    let msgText = 'msg' + new Date().getTime();
-
-    it.skip('create message', () => {
-        cy.loginAdmin();
-        cy.createMessage(msgText);
-    })
-
-});
