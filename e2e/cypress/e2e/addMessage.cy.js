60a4dbcbdf302911279d53ec8eb3ffb87761bc45
Sebastian Schuch <e12222156@student.tuwien.ac.at>
2024-06-25 12:04:31 +0200

    feat: added an e2e test for logging in

--- e2e/cypress/e2e/addMessage.cy.js
+++ e2e/cypress/e2e/addMessage.cy.js
@@ -1,9 +1,23 @@
-context('add message', () => {
-    let msgText = 'msg' + new Date().getTime();
+describe('Login Test', () => {
+    it('should navigate to login page, enter credentials and submit', () => {
+        // Open the website
 
-    it.skip('create message', () => {
-        cy.loginAdmin();
-        cy.createMessage(msgText);
-    })
+        // Navigate to login page
+        cy.visit('/#/login');
 
+        cy.wait(1000);
+
+        // Enter email
+        cy.get('form .form-group #inputEmail').type('chris.anger@email.com');
+
+        // Enter password
+        cy.get('input[name=password]').type('AngerManagement');
+
+        // Submit the form
+        cy.contains('button span', ' Login ').click();
+
+        cy.wait(1000);
+
+        cy.get('div.user-name').should('contain', 'Chris A');
+    });
 });
